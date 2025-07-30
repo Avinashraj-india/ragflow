d0e3652dc9d027a6aae6d0087d7560ea7ddddec3
Avinash Raj <aviraj@WKMZT0ED75E0.global.publicisgroupe.net>
2025-07-30 06:42:41 +0530

    Changes done

--- rag-ui/src/App.jsx
+++ rag-ui/src/App.jsx
@@ -1,16 +1,26 @@
+// App.jsx
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignInSide from './components/sign-in-side/SignInSide.tsx';     // 👈 your login component
 import Sidebar from "./components/Sidebar";
-import ChatWindow from "./components/ChatWindow";
 import ChatBox from "./components/ChatBox/";
 
 function App() {
   return (
-    <div className="h-screen flex bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 flex flex-col justify-between items-center p-4">
-        <ChatBox /> {/* 👈 Add ChatBox directly */}
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<SignInSide />} />
+      <Route
+        path="/chat"
+        element={
+          <div className="h-screen flex bg-gray-100">
+            <Sidebar />
+            <div className="flex-1 flex flex-col justify-between items-center p-4">
+              <ChatBox />  {/* ✅ This will load exactly what you wrote */}
+            </div>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
